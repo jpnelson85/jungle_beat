9926e86db185b9707eb5ae78c3058f19c33d2322
Jeff Nelson <jpnelson85@gmail.com>
2023-02-03 16:37:32 -0600

    update nodes

--- lib/node.rb
+++ lib/node.rb
@@ -1,12 +1,12 @@
 class Node
+  attr_reader = 
 
   def initialize(data)
     @data = data
   end
-  
-  def next_node
-      @next_node = nil
-  end
 
+  def next_node
+    node = nil
+  end
 end
 
