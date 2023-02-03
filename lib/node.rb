f78ea049eb886bfebda2f51e4f434fd0f659cbe9
Jeff Nelson <jpnelson85@gmail.com>
2023-02-03 16:19:21 -0600

    add node basics

---
+++ lib/node.rb
@@ -0,0 +1,12 @@
+class Node
+
+  def initialize(data)
+    @data = data
+  end
+  
+  def next_node
+      @next_node = nil
+  end
+
+end
+
---
+++ spec/node_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/node.rb'
+
+describe Node do
+  it 'a node exists' do
+    node = Node.new('plop')
+    expect(node).to be_a(Node)
+  end
+
+  it 'next_node equals nil as default' do
+    node = Node.new('plop')
+    expect(node.next_node).to eq(nil)
+  end
+  
+end
