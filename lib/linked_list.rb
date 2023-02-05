1bdf7b988c6cbe9e837ee3fb37c99254b4b38cf0
Jeff Nelson <jpnelson85@gmail.com>
2023-02-05 12:46:18 -0600

    add count method

---
+++ lib/linked_list.rb
@@ -0,0 +1,43 @@
+require './lib/node'
+
+class LinkedList
+  attr_accessor :head
+
+  def initialize
+    @head = nil
+  end
+
+  def append(sound)
+    if @head = nil?
+      @head = Node.new(data, nil)
+    else
+      last_node = @head
+      while(!last_node.next_node.nil?)
+        last_node = last_node.next_node
+      end
+    
+      last_node.next_node = Node.new(data, nil)
+    end
+  end
+
+  def count
+    count = 0
+    node = @head
+    while(!node.nil?)
+      count += 1
+      node = node.next_node
+    end
+    return count
+  end
+
+  def find(data)
+    node = @head
+    while(!node.nil?)
+      if(node.value == data)
+        return true
+      end
+      node = node.next_node
+    end
+      false
+end
+end
